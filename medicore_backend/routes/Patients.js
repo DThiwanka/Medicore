cd0052209c88f4f7c25930cf8b4a5ef888dbcc9e
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-12-08 23:17:51 +0530

    add patient

--- medicore_backend/routes/Patients.js
+++ medicore_backend/routes/Patients.js
@@ -1,3 +1,34 @@
 const router = require("express").Router();
 
 let Patient = require('../models/Patients');
+
+router.route('/add').post((req,res) => {
+    
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const gender = req.body.gender;
+    const bloodGroup = req.body.bloodGroup;
+    const address = req.body.address;
+    const notes = req.body.notes;
+
+    const newPatient = new Patient({
+
+        name,
+        email,
+        password,
+        gender,
+        bloodGroup,
+        address,
+        notes
+    })
+
+    newPatient.save().then(() => {
+        res.json("Patient Added")
+    }).catch((err)=>{
+        console.log(err); 
+    })
+
+});
+
+module.exports = router;
