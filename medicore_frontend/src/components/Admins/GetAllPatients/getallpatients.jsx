64a66bbb56d4312225a8e35dcdeb55f05e61d540
DThiwanka <it20129576@my.sliit.lk>
2024-02-12 18:50:25 +0530

    Add react-to-print library for PDF generation

--- medicore_frontend/src/components/Admins/GetAllPatients/getallpatients.jsx
+++ medicore_frontend/src/components/Admins/GetAllPatients/getallpatients.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, generatePath } from 'react-router-dom';
 import { BsCalendar2CheckFill } from "react-icons/bs";
 
 
+import { useReactToPrint } from 'react-to-print';
+import { Button } from 'react-bootstrap';
 
 const internalTable = {
   fontFamily: 'Helvetica',
-  maxWidth: '70%', 
+  maxWidth: '70%',
   margin: '0 auto',
   backgroundColor: '#f5f5f5',
   border: '1px solid #ddd',
@@ -27,6 +29,9 @@ const tableHeaderStyle = {
 
 //Functions
 function Getallpatients() {
+
+  const componentPDF = useRef();
+
   const [patients, setPatients] = useState([]);
 
   useEffect(() => {
@@ -66,14 +71,22 @@ function Getallpatients() {
 
   };
 
-  const OnViewApp = async (userId) => { 
+  const OnViewApp = async (userId) => {
     window.location.href = `/admin/view/appointment/${userId}`;
   }
 
+  const generatePDF = useReactToPrint({
+    content: () => componentPDF.current,
+    documentTitle: 'Patient Details',
+    copyStyles: false,
+    onAfterPrint: () => { 
+      alert('PDF Generated Successfully');
+    }
+  })
 
   return (
     <div>
-      
+
       <div>
 
         {/* <button style={{ borderRadius: "5px", background: "#b30059", padding: "0.5%" }}>
@@ -91,18 +104,19 @@ function Getallpatients() {
       </center>
       <br />
       <div className="mb-1">
+        <div ref={componentPDF} style={{width:'100%'}}>
         <table className="table table-bordered table-hover" style={internalTable}>
           <thead style={tableHeaderStyle}>
             <tr>
               <th scope="col">Id</th>
               <th scope="col">Name</th>
               <th scope="col">Email</th>
-              
+
               <th scope="col">Gender</th>
               <th scope="col">BloodGroup</th>
               <th scope="col">Address</th>
-              
-              <th scope="col" colSpan={3} style={actionCellStyle}>Action</th>
+
+              <th scope="col" colSpan={3} id='#act' style={actionCellStyle}>Action</th>
             </tr>
           </thead>
 
@@ -127,7 +141,13 @@ function Getallpatients() {
               </tr>
             ))}
           </tbody>
-        </table>
+          </table>
+        </div>
+        <div className='d-grid d-md-flex justify-content-md-end mb-3'>
+          <Button className='btn btn-success' onClick={generatePDF}>
+            PDF
+          </Button>
+        </div>
       </div>
     </div>
   );
